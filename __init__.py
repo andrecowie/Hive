8cb14d688b052907703b9714be09622181b9dcdd
Andre Cowie <drecowie@gmail.com>
2018-05-26 16:21:25 +1200

    Lel

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from flask import Flask, url_for, redirect, session, render_template, request, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("svgs.html")
+
+app.secret_key = "62e264ed-873c-474b-b0a8-cb2a7a603f74"
+
+if __name__ == "__main__":
+    app.run(debug=True)
